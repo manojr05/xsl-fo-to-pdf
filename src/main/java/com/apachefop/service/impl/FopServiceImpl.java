b3d508478601276667bd3695b9ef4d78f4e9a72a
Manoj R <manoj.r@diatoz.com>
2024-04-04 16:21:40 +0530

    made changes to return png

--- src/main/java/com/apachefop/service/impl/FopServiceImpl.java
+++ src/main/java/com/apachefop/service/impl/FopServiceImpl.java
@@ -1,7 +1,6 @@
 package com.apachefop.service.impl;
 
 import com.apachefop.service.FopService;
-import com.apachefop.utils.PdfToJpgConverter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.fop.apps.Fop;
@@ -14,11 +13,13 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import javax.xml.transform.*;
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.sax.SAXResult;
 import javax.xml.transform.stream.StreamSource;
 import java.io.*;
-import java.util.List;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -32,46 +33,36 @@ public class FopServiceImpl implements FopService {
         File foFile = resourceLoader.getResource("classpath:template.fo").getFile();
 
         try (FileInputStream foInputStream = new FileInputStream(foFile);
-             ByteArrayOutputStream jpgOutputStream = new ByteArrayOutputStream()) {
+             ByteArrayOutputStream pngOutputStream = new ByteArrayOutputStream()) {
 
-            // Read the FO template as a string
             String foTemplate;
             try (BufferedReader reader = new BufferedReader(new InputStreamReader(foInputStream))) {
                 foTemplate = reader.lines().collect(Collectors.joining("\n"));
             }
 
-            // Replace placeholders with actual values
             String transformedFO = foTemplate
                     .replace("${title}", "Manoj R")
                     .replace("${description}", "SDE")
                     .replace("${content}", "DIATOZ Solutions pvt ltd");
 
-            // Transform the transformed FO template
             Source foSource = new StreamSource(new StringReader(transformedFO));
 
-            // Create FOP and generate PDF
             FopFactory fopFactory = FopFactory.newInstance(new File(".").toURI());
-            Fop fop = fopFactory.newFop(MimeConstants.MIME_PDF, jpgOutputStream);
+            Fop fop = fopFactory.newFop(MimeConstants.MIME_PNG, pngOutputStream);
 
             TransformerFactory factory = TransformerFactory.newInstance();
             Transformer transformer = factory.newTransformer();
             Result res = new SAXResult(fop.getDefaultHandler());
 
-            // Transform the FO source and generate PDF
             transformer.transform(foSource, res);
-            jpgOutputStream.close();
 
-            // Get the generated PDF bytes
-            byte[] pdfBytes = jpgOutputStream.toByteArray();
-            PdfToJpgConverter.convertPdfToJpg(pdfBytes);
+            byte[] pngBytes = pngOutputStream.toByteArray();
 
-            // Set HTTP headers
             HttpHeaders headers = new HttpHeaders();
-            headers.setContentType(MediaType.APPLICATION_PDF);
-            headers.setContentLength(pdfBytes.length);
+            headers.setContentType(MediaType.IMAGE_PNG);
+            headers.setContentLength(pngBytes.length);
 
-            // Return the PDF bytes as ResponseEntity
-            return new ResponseEntity<>(pdfBytes, headers, HttpStatus.OK);
+            return new ResponseEntity<>(pngBytes, headers, HttpStatus.OK);
         }
     }
 }
